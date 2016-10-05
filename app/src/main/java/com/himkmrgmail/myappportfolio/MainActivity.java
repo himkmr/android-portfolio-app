b643f259c33eea38ba7f70350a3e919035b77a18
himanshu verma <himanshu verma>
2016-10-04 20:14:56 -0500

    portfolio_v01.01 basic functionalities complete

--- app/src/main/java/com/himkmrgmail/myappportfolio/MainActivity.java
+++ app/src/main/java/com/himkmrgmail/myappportfolio/MainActivity.java
@@ -27,11 +27,36 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-    public void onButtonClick()
-    {
+    public void onButtonClick(View view) {
         Toast toast;
-        toast = Toast.makeText(getApplicationContext(), "This will launch my first app", Toast.LENGTH_SHORT);
+        switch (view.getId()) {
+            case (R.id.button_bigger):
+                toast = Toast.makeText(getApplicationContext(), "This button will launch my Build It Bigger app", Toast.LENGTH_SHORT);
+                toast.show();
+                break;
+            case (R.id.button_cap):
+                toast = Toast.makeText(getApplicationContext(), "This will launch my Capstone app", Toast.LENGTH_SHORT);
+                toast.show();
+                break;
+            case (R.id.button_hawk):
+                toast = Toast.makeText(getApplicationContext(), "This will launch my Stock Hawk app", Toast.LENGTH_SHORT);
+                toast.show();
+                break;
+            case (R.id.button_material):
+                toast = Toast.makeText(getApplicationContext(), "This will launch my Material app", Toast.LENGTH_SHORT);
+                toast.show();
+                break;
+            case (R.id.button_movie):
+                toast = Toast.makeText(getApplicationContext(), "This will launch my Movies app", Toast.LENGTH_SHORT);
+                toast.show();
+                break;
+            case (R.id.button_ub):
+                toast = Toast.makeText(getApplicationContext(), "This will launch my Ubiquitous app", Toast.LENGTH_SHORT);
+                toast.show();
+                break;
+        }
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
